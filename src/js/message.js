49c9f136d0e27a4baf480033e2cc6ce892e624be
R-TEK <doughtycallum.1999@gmail.com>
2020-06-26 20:03:35 +0100

    Added media queries for smaller screen devices
    Added pointer events so the message container does not prevent clicking
    on elements

--- src/js/message.js
+++ src/js/message.js
@@ -62,9 +62,9 @@ Alertpal.message = function (type, details) {
 	}, timeout);
 
 	// Timeout to trigger the removal of the element in the DOM
-	setTimeout(function () {
-		container.removeChild(messageNode);
-	}, timeout + 200);
+	//	setTimeout(function () {
+	//		container.removeChild(messageNode);
+	//	}, timeout + 200);
 
 	// Checking whether to append or insertBefore
 	if (container.children.length < 1) {
